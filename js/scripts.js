bdf16d26ac502f1539485a885d721451b50ea659
Peter Kingori <kpndegwa@gmail.com>
2019-07-29 00:21:00 +0300

    Add various attributes to the ordering form so that the selected values of the pizza size and ingredients can be picked by the JS interface logic. Functionality has also been added in the scripts file using jQuery to collect user input. It has been tested in the JS console and it works.

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,21 @@
+//business logic
+
 // user interface logic
 $(document).ready(function() {
 	$('button#order-clicked').click(function() {
 		$('form#order-form').show();
 	});
 	
-	
+	$('form#order-form').submit(function(event) {
+		event.preventDefault();
+		
+		var size = $('input:radio[name="pizza-size"]:checked').val();
+		var crust = $('input:radio[name="pizza-crust"]:checked').val();
+		var toppings = [];
+				$('input:checkbox[name="toppings"]:checked').each(function() {
+					toppings.push(this.value);
+				});
+	})
 	
 	
 	
